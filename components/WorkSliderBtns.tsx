78642b8e165357e3f1d454200f9eb2ab4c30ff9d
rr4180885 <rr4180885@gmail.com>
2024-10-13 14:44:19 +0530

    fix-childproblem-7

--- components/WorkSliderBtns.tsx
+++ components/WorkSliderBtns.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import {useSwiper} from "swiper/react"
-import {PiCaretLeftBold, PiCaretRightBold}  from "react-icons/pi"
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-const WorkSliderBtn = ({containerStyles,btnStyles, iconStyles}) =>{
-    const swiper = useSwiper();
-    console.log("current swiper instance:", swiper);
-    return (
-        <div className={containerStyles}>
-            <button className={btnStyles} onClick={()=>swiper.slidePrev()}>
-                <PiCaretLeftBold className={iconStyles}/>
-            </button>
-            <button className={btnStyles} onClick={()=>swiper.slideNext()}>
-                <PiCaretRightBold className={iconStyles}/>
-            </button>
-        </div>
-    );
+interface WorkSliderBtnProps {
+  containerStyles: string;
+  btnStyles: string;
+  iconStyles?: string; // Optional prop for icon styles
+}
+
+const WorkSliderBtn: React.FC<WorkSliderBtnProps> = ({ containerStyles, btnStyles, iconStyles }) => {
+  const swiper = useSwiper();
+  console.log("current swiper instance:", swiper);
+
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold className={iconStyles} />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold className={iconStyles} />
+      </button>
+    </div>
+  );
 }
 
 export default WorkSliderBtn;
